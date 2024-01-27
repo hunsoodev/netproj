b5784e82640426e99ee5a8e6bf97ad810c2e3eaf
Hun Soo Jeong <147886939+hunsoodev@users.noreply.github.com>
2024-01-27 23:44:02 +0900

    Update Renewable_energy_generation.py

--- dags/Renewable_energy_generation.py
+++ dags/Renewable_energy_generation.py
@@ -30,8 +30,9 @@ REDSHIFT_SCHEMA = "raw_data"
 REDSHIFT_TABLE = "renewable_energy"
 
 # raw_daya에 저장할 데이터의 형식
-SQL_STRING = f"DELETE FROM {REDSHIFT_DATABASE}.{REDSHIFT_SCHEMA}.{REDSHIFT_TABLE};"
-CREATE_TABLE_SQL_STRING = f"""CREATE TABLE IF NOT EXISTS {REDSHIFT_DATABASE}.{REDSHIFT_SCHEMA}.{REDSHIFT_TABLE} (
+# SQL_STRING = f"DELETE FROM {REDSHIFT_DATABASE}.{REDSHIFT_SCHEMA}.{REDSHIFT_TABLE};"
+CREATE_TABLE_SQL_STRING = f"""
+CREATE TABLE IF NOT EXISTS {REDSHIFT_DATABASE}.{REDSHIFT_SCHEMA}.{REDSHIFT_TABLE} (
         date TIMESTAMP,
         generation FLOAT,
         generator_name VARCHAR(50)
@@ -278,7 +279,7 @@ transform = PythonOperator(
 create_table = PostgresOperator(
     task_id="create_table",
     postgres_conn_id="netproj_redshift_conn_id",
-    sql=SQL_STRING+CREATE_TABLE_SQL_STRING,
+    sql=CREATE_TABLE_SQL_STRING,
     dag=dag,
 )
 
